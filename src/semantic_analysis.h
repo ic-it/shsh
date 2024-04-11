44e7857d9729c24effb559b2d54da8abf792fb45
Illia Chaban <ic-it@mail.com>
2024-04-11 17:08:54 +0200

    feat: new semantic rules

--- src/semantic_analysis.h
+++ src/semantic_analysis.h
@@ -11,11 +11,13 @@ typedef enum {
 const char *error_reasons[] = {
     "TCP IN/OUT and FILE IN/OUT cannot be used together",
     "OUT to the file/tcp and PIPE cannot be used together",
+    "PIPE and BACKGROUND cannot be used together",
 };
 
 typedef enum {
   REASON_IO_CONFLICT,
   REASON_PIPE_CONFLICT,
+  REASON_BACKGROUND_CONFLICT,
 } ReasonEnum;
 
 typedef struct {
@@ -38,6 +40,10 @@ SemanticResult semantic_analyze(Command *command) {
   if ((command->flags & (CMD_TCP_OUT | CMD_FILE_OUT)) &&
       (command->flags & CMD_PIPE)) {
     return (SemanticResult){.reason = REASON_PIPE_CONFLICT,
+                            .result = SEMANTIC_ERROR};
+  }
+  if ((command->flags & CMD_PIPE) && (command->flags & CMD_BG)) {
+    return (SemanticResult){.reason = REASON_BACKGROUND_CONFLICT,
                             .result = SEMANTIC_ERROR};
   }
   return (SemanticResult){.result = SEMANTIC_OK};
